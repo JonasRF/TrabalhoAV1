cae064aae2adcc11279e3ac43b53116865c6846f
jonas ribeiro <jonasribeiro41@yahoo.com.br>
2023-09-30 10:20:34 -0300

    Projeto AV1

---
+++ src/projetoav1/ProjetoAV1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projetoav1;
+
+import projetoav1.gui.MainView;
+
+/**
+ *
+ * @author jonas
+ */
+public class ProjetoAV1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new MainView().setVisible(true);
+    }
+}
